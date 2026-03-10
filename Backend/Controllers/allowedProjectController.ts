b3b3b98b5c31773efa92937f61d746576d67f683
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-10 22:54:17 +0500

    Fix: added import of type request and response

--- Backend/Controllers/allowedProjectController.ts
+++ Backend/Controllers/allowedProjectController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import { b24 } from "../Auth/bitrix24AuthUtil.js";
 import { logger } from "../Utils/logger.js";
 
