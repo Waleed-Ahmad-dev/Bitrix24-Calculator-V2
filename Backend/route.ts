9bce59e916f585570deec23d91ce5e8c80ae7eb8
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-11 20:16:46 +0500

    Feat: added the controller for getting the readable text version

--- Backend/route.ts
+++ Backend/route.ts
@@ -2,6 +2,7 @@ import { getAllowedProjects } from "./Controllers/allowedProjectController.js";
 import { getListProperties} from "./Controllers/listPropertiesController.js";
 import { getCatalogProductWithFilters } from "./Controllers/catalogController.js";
 import { getProductData } from "./Controllers/productController.js";
+import { getReadableTextOfProperties } from "./Controllers/readableTextOfPropertiesController.js";
 
 export const setupRoutes = (app: any) => {
     app.post('/allowed-projects', getAllowedProjects);
@@ -11,4 +12,6 @@ export const setupRoutes = (app: any) => {
     app.post('/catalog-products', getCatalogProductWithFilters);
 
     app.post('/product', getProductData);
+
+    app.post('/readable-text-of-properties', getReadableTextOfProperties);
 }
