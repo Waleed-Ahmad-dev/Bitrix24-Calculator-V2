a9e65c961b13e348bc200680b40171205339edde
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-11 22:01:24 +0500

    Fix: added fetching for the properties

--- Frontend/scripts/Bitrix24HelperFunctions/getTheListProperties.js
+++ Frontend/scripts/Bitrix24HelperFunctions/getTheListProperties.js
@@ -3,15 +3,41 @@ import { callBX24Method } from "./callBX24Method.js";
 
 export const getListProperties = async(propertyID) => {
 
-    try{
-        const result = await callBX24Method('catalog.productPropertyEnum.list', {filter: {propertyId: propertyID}});
+    // try{
+    //     const result = await callBX24Method('catalog.productPropertyEnum.list', {filter: {propertyId: propertyID}});
 
-        console.log(`List Properties for property ID: ${propertyID}`, result);
-        return result;
+    //     console.log(`List Properties for property ID: ${propertyID}`, result);
+    //     return result;
+    // }
+    // catch(error){
+    //     console.error('Error fetching list properties:', error);
+    //     throw error;
+    // }
+
+    try {
+        const response = await fetch("https://calcenchancev2.premierchoiceint.online/list-properties", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                propertyId: propertyID,
+            }),
+        });
+
+
+        if(!response.ok){
+            throw new Error(`Failed to fetch the properties for ${propertyID}` )
+        }
+
+
+        const data = await response.json();
+
+        return data.propertiesData.productPropertyEnums || null;
     }
-    catch(error){
+    catch (error) {
         console.error('Error fetching list properties:', error);
-        throw error;
+        return null;
     }
 
  
