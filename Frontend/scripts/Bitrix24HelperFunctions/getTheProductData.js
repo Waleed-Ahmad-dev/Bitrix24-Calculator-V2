148b8211248d500d35d0c687793fe58fc836e26a
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-11 22:59:23 +0500

    feat: added the product data fetching through server]

--- Frontend/scripts/Bitrix24HelperFunctions/getTheProductData.js
+++ Frontend/scripts/Bitrix24HelperFunctions/getTheProductData.js
@@ -1,12 +1,40 @@
 import { callBX24Method } from "./callBX24Method.js";
 
 export const getTheProductData = async (productId) => {
-    try {
-        const productData = await callBX24Method('crm.product.get', { id: productId });
-        return productData;
+  //     try {
+  //         const productData = await callBX24Method('crm.product.get', { id: productId });
+  //         return productData;
+  //     }
+  //     catch (error) {
+  //         console.error("Error fetching product data:", error);
+  //         return null;
+  //     }
+
+  try {
+
+    const response = await fetch(
+      "https://calcenchancev2.premierchoiceint.online/product",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          productId: productId,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+        throw new Error("Error while fetching the product data")
     }
-    catch (error) {
-        console.error("Error fetching product data:", error);
-        return null;
-    }
-}
+
+    const data = await response.json();
+
+    return data.product || null;
+
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+    return null;
+  }
+};
