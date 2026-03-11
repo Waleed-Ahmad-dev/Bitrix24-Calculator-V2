93e3c9ecceb7212294144b81a3d8cfcdae39b273
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-11 21:16:36 +0500

    Feat: added the server call for the project list

--- Frontend/scripts/Bitrix24HelperFunctions/getProjectList.js
+++ Frontend/scripts/Bitrix24HelperFunctions/getProjectList.js
@@ -2,15 +2,36 @@ import { callBX24Method } from "./callBX24Method.js"
 
 export const getProjectList = async () => {
 
+    // try {
+    //     const projects = await callBX24Method('catalog.productPropertyEnum.list', {filter: {propertyId: 173}});
+
+    //     console.log('Projects:', projects);
+
+    //     return projects;
+    // }
+    // catch (error){
+    //     console.error('Error fetching projects:', error);
+    // }
+
+
     try {
-        const projects = await callBX24Method('catalog.productPropertyEnum.list', {filter: {propertyId: 173}});
+        const response = await fetch("https://calcenchancev2.premierchoiceint.online/all-projects", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await response.json();
+
+        const projects = data.projectData.productPropertyEnums || [];
 
         console.log('Projects:', projects);
-
         return projects;
     }
-    catch (error){
+    catch (error) {
         console.error('Error fetching projects:', error);
+        return [];
     }
 
 
