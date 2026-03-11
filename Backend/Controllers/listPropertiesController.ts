fada1b780e2898b3ae9b8fb6dc08575f55c25b55
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-03-11 14:24:46 +0500

    Fix: added the list properties api

---
+++ Backend/Controllers/listPropertiesController.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from 'express';
+import { b24 } from '../Auth/bitrix24AuthUtil.js';
+import { logger } from '../Utils/logger.js';
+
+
+export const getListProperties = async (req: Request, res: Response): Promise<void> => {
+    const client = b24.instance;
+    const propertyId = req.body.propertyId;
+
+    if (!propertyId) {
+        res.status(400).json({ message: "Property ID is required" });
+        return;
+    }
+
+    try {
+        const propertiesResponse = await client.actions.v2.callList.make({method: 'catalog.productPropertyEnum.list', params: {filter: {PROPERTY_ID: propertyId}}, idKey: 'ID', });
+        const propertiesData = propertiesResponse.getData() || [];
+
+        logger.info('Fetched properties from Bitrix24', { propertiesData });
+        res.json({ properties: propertiesData });
+    } catch (error) {
+        logger.error('Error fetching properties from Bitrix24', { error });
+        res.status(500).json({ message: "Internal server error" });
+
+    }
+
+
+}
--- Backend/route.ts
+++ Backend/route.ts
@@ -1,5 +1,8 @@
 import { getAllowedProjects } from "./Controllers/allowedProjectController.js";
+import { getListProperties} from "./Controllers/listPropertiesController.js";
 
 export const setupRoutes = (app: any) => {
     app.post('/allowed-projects', getAllowedProjects);
+
+    app.post('/list-properties', getListProperties);
 }
